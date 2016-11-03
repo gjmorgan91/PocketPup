829cd26cb1da4b4536e05b72da03e8ce4a3cdbd0
Greg Morgan <gjmorgan@ucsd.edu>
2016-11-03 16:33:41 -0700

    voting js

--- static/js/vote.js
+++ static/js/vote.js
@@ -9,12 +9,13 @@ function intializePage() {
 	$('#thumbsDown').hide();
 	$('#thumbsUp').hide();
 	$('.voteText').hide();
+	$('.voteSubmit').hide();
 
 	var like = true;
 	var dislike = true;
 	var inputGiven = false;
+	var submitThis = false;
 
-	//add functionality that makes it not possible to submit until voting
 	//get more pictures to make the valid submit change to another image
 	//determine how to store the vote to the picture
 	//page will need to load one of the preset profiles and images
@@ -29,7 +30,10 @@ function intializePage() {
 			like = false;
 			dislike = true;
 		}
-
+		if (submitThis == false) {
+			submitThis = true;
+			$('.voteSubmit').show();
+		}
 	}
 
 	$('#down').click(DownVote);
@@ -42,12 +46,31 @@ function intializePage() {
 			dislike = false;
 			like = true;
 		}
+		if (submitThis == false) {
+			submitThis = true;
+			$('.voteSubmit').show();
+		}
 	}
 
 	$('#comment').click(showComment);
 	function showComment(event) {
 		$('.voteText').show();
 		window.scrollTo(0,5000);
+		if (submitThis == false) {
+			submitThis = true;
+			$('.voteSubmit').show();
+		}
+	}
+
+	$('.report').click(reportThis);
+	function reportThis(event) {
+		$.get("../data.json", updateVote());
+	}
+
+	function updateVote(result) {
+		console.log(result);
+		
+
 	}
 
 }
