1b924be8d7c6a9b98b81f280f3f114ccefb78c35
Jacob Aguirre <jaguirre1231@gmail.com>
2016-11-10 13:40:52 -0800

    more sign up shit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,38 @@
+var dog = {	"avatar": "images/dogs/swarley.png",
+			"breed": "Golden Retriever"}
+
 function nextDog() {
 	$.get("signup/getDog", nextDogUpdate);
+	$('#signupDog').hide();
+	$('#signupBreed').hide();
 }
 
 function  nextDogUpdate(result) {
 	var breed = result['breed'];
 
-	var dog = {	"avatar": "images/dogs/swarley.png",
-	"breed": "Golden Retriever"}
 
 	if (breed == "Golden Retriever") {
 		dog = {	"avatar": "images/dogs/poodle.png",
 				"breed": "Poodle"};
-		$.post("signup/update", dog);
 	}
 	else if (breed == "Poodle") {
 		dog = {	"avatar": "images/dogs/husky.png",
 				"breed": "Husky"};
-		$.post("signup/update", dog);
 	}
 	else if (breed == "Husky") {
 		dog = {	"avatar": "images/dogs/terrier.png",
 				"breed": "Jack Terrier"};
-		$.post("signup/update", dog);
 	}
 	else if (breed == "Jack Terrier") {
 		dog = {	"avatar": "images/dogs/swarley.png",
 				"breed": "Golden Retriever"};
-		$.post("signup/update", dog);
 	}
+
+	$.post({url: "signup/update", data: dog, success: showDog()});
+}
+
+function showDog() {
+	//setTimeout(function() {location.reload();}, 0);
 }
 
 function prevDog() {
@@ -36,40 +41,39 @@ function prevDog() {
 
 function  prevDogUpdate(result) {
 	var breed = result['breed'];
-	var dog = {	"avatar": "images/dogs/swarley.png",
-	"breed": "Golden Retriever"}
 
 	if (breed == "Goldren Retriever") {
 		dog = {	"avatar": "images/dogs/terrier.png",
 				"breed": "Jack Terrier"};
-		$.post("signup/update", dog);
-
 	}
 	else if (breed == "Poodle") {
 		dog = {	"avatar": "images/dogs/swarley.png",
 				"breed": "Golden Retriever"};
-		$.post("signup/update", dog);
-
 	}
 	else if (breed == "Husky") {
 		dog = {	"avatar": "images/dogs/poodle.png",
 				"breed": "Poodle"};
-		$.post("signup/update", dog);
-
 	}
 	else if (breed == "Jack Terrier") {
 		dog = {	"avatar": "images/dogs/husky.png",
 				"breed": "Husky"};
-		$.post("signup/update", dog);
-
 	}
+
+	$.post("signup/update", dog);
 }
 
 function submitProf() {
-	var prof = { "ownerName": $('#signupOwner').val(),
-					"dogName": $('#signupDogName').val(),
-					"age": $('#signupAge').val(), }
-	$.post({url: "signup/updateProfile", data: prof, success: logIn()});
+	if (($('#signupOwner').val() == '') || ($('#signupDogName').val() == '') || ($('#signupAge').val() == '')) {
+		$('body').append("<h5 id='notFilled' font-size='2em'>Please fill out all forms</h5>");
+		setTimeout(function(){$('#notFilled').remove();}, 2000);
+	}
+
+	else {
+		var prof = { "ownerName": $('#signupOwner').val(),
+						"dogName": $('#signupDogName').val(),
+						"age": $('#signupAge').val()}
+		$.post({url: "signup/updateProfile", data: prof, success: logIn()});
+	}
 }
 
 function logIn() {
