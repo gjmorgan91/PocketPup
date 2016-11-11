cbbb924c1d2b4e6cd3819e6d6dcd0f5e2cb0ce54
Greg Morgan <gjmorgan@ucsd.edu>
2016-11-10 23:37:31 -0800

    store kinda working but not really k

--- routes/store.js
+++ routes/store.js
@@ -1,11 +1,12 @@
-var storePage = require('../store.json');
+var storePage = require('../store2.json');
 var profile = require('../profiles.json');
 
 var currStore = 0;
 
-exports.view = function(req, res){
-	var currStoreItems = JSON.stringify(storePage.indoor);
-	res.render('store', currStoreItems);
+exports.view = function(req, res) {
+	// var currStoreItems = JSON.parse({storePage:storePage});
+	console.log({storePage:storePage});
+	res.render('store', {storePage:storePage});
 }
 
 /*exports.view = function(req, res){
