56a6c73a88604e738c1600feeaf0bf75e67f377d
Jacob Aguirre <jaguirre1231@gmail.com>
2016-11-16 15:01:14 -0800

    Made vote randomize next profile. Added bones to the page.

--- routes/vote.js
+++ routes/vote.js
@@ -1,22 +1,25 @@
 var voters = require('../voteData.json');
 var profile = require('../profiles.json');
 
-var index = 1;
+var index = 2;
 
 exports.view = function(req, res) {
 	index = parseInt(voters[0].index);
+	voters[index].cashmonkey = profile[0].cashmonkey;
 	res.render('vote', voters[index]);
 }
 
 exports.setIndex = function(req, res) {
-	if (index == 6)
-		index = 1
-	else
-		index++
-	voters[0].index = index.toString();
+	while(true){
+		var newIndex = Math.floor((Math.random() * 6) + 2);
+		if (newIndex != index) {
+			index = newIndex;
+			break;
+		}
+	}
+		voters[0].index = index.toString();
 }
 
 exports.giveBones = function(req, res) {
 	profile[0].cashmonkey = parseInt(profile[0].cashmonkey) + parseInt(req.body.boneValue);
-	console.log(profile[0].bones);
 }
