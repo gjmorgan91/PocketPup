c3ef734501c64f10bab330203f19be5678496d03
Greg Morgan <gjmorgan@ucsd.edu>
2016-11-16 21:55:16 -0800

    bowlz

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,19 @@
 'use strict';
 
+var amHungry = true;
+var hour = null;
+var date = null;
+
  $(document).ready(function() {
-     $('#inside').hide();
+ 	$('#bowlOutdoors').hide();
+ 	if (amHungry == false) {
+ 		$('#bowlEmpty1').hide();
+ 		$('#bowlEmpty2').hide();
+ 	} else {
+ 		$('#bowlFull1').hide();
+ 		$('#bowlFull2').hide();
+ 	}
+    $('#inside').hide();
  });
 
 function BGChange() {
@@ -9,6 +21,8 @@ function BGChange() {
     document.getElementById("background").src = 'images/bg2.png';
     $('#outside').hide("fast");
     $('#inside').show("fast");
+    $('#bowlIndoors').hide();
+    $('#bowlOutdoors').show();
 }
 
 function BGChangeBack() {
@@ -16,4 +30,20 @@ function BGChangeBack() {
     document.getElementById("background").src = 'images/bg1.png';
     $('#outside').show("fast");
     $('#inside').hide("fast");
+    $('#bowlIndoors').show();
+    $('#bowlOutdoors').hide();
+}
+
+function updateFood() {
+	if (amHungry == true) {
+		$('#bowlEmpty1').hide();
+		$('#bowlFull1').show();
+		$('#bowlEmpty2').hide();
+		$('#bowlFull2').show();
+	}
+}
+
+function myFunction() {
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
 }
